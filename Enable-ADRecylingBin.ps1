5fd8d256600486867df1a37f0ce5e6ffd422ec5b
Cody Aldrich <57390775+coaldric@users.noreply.github.com>
2022-12-02 23:26:32 +0100

    Add files via upload

---
+++ Enable-ADRecylingBin.ps1
@@ -0,0 +1,6 @@
+﻿#set variables
+$domain = Get-ADDomain
+$domaindn = $domain.DistinguishedName
+$domainname = $domain.name
+
+Enable-ADOptionalFeature –Identity "CN=Recycle Bin Feature,CN=Optional Features,CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,$domaindn" -Scope ForestOrConfigurationSet -Target "$domainname"
---
+++ Import-WMIfilters.ps1
@@ -0,0 +1,13 @@
+﻿$domain = (Get-ADDomainController).Domain
+
+$wmiFilters = (Get-ChildItem -Path "C:\Temp\Oct 22 DISA STIG GPO Package 1012\mof").FullName
+
+foreach ($wmiFilter in $wmiFilters) {
+            (Get-Content $WMIFilter) -replace "security.local", "$domain" | Set-Content $WMIFilter 
+            (Get-Content $WMIFilter) -replace "gpoimport.local", "$domain" | Set-Content $WMIFilter 
+
+            # Import WMI Filters
+            Write-Host -ForegroundColor Green "`t`tImporting WMI Filters"
+            $null = mofcomp -N:root\Policy $WMIFilter
+           
+}
